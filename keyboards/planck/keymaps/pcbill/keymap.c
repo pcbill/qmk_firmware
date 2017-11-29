6ae0881cb53ff748ee3ad03f614d78a10495c7fb
Bill SJ Wu <Bill_SJ_Wu@wistron.com>
2017-11-29 15:59:25 +0800

    change ralt,app to pgup, pudn

--- keyboards/planck/keymaps/pcbill/keymap.c
+++ keyboards/planck/keymaps/pcbill/keymap.c
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Alt  |  App | Ctrl |
+ * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Pg Up|Pg Dn | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,   KC_RALT, KC_APP,  KC_RCTL}
+  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,   KC_PGUP, KC_PGDN, KC_RCTL}
 },
 
 /* Colemak
@@ -68,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Alt  |App   | Ctrl |
+ * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Pg Up|Pg Dn | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
   {KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT },
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,   KC_RALT, KC_APP,  KC_RCTL}
+  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,   KC_PGUP, KC_PGDN, KC_RCTL}
 },
 
 /* Dvorak
@@ -86,14 +86,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Alt  |   App| Ctrl |
+ * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Pg Up|Pg Dn | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
   {KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT },
   {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT},
-  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,   KC_RALT, KC_APP,  KC_RCTL}
+  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER, KC_PGUP, KC_PGDN,   KC_RCTL}
 },
 
 /* Lower
@@ -104,14 +104,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   F12|  F7  |  F8  |  F9  | Vol- |BL_DEC|Home  |insert|  End |      | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Alt  |   App| Ctrl |
+ * | Esc  | GUI  | Alt  | Lower|Space |  Shift      |Raise | Lower| Pg Up|Pg Dn | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
   {KC_TAB,  KC_F10,  KC_F1,   KC_F2,   KC_F3,  KC_MUTE,  BL_TOGG, KC_PGUP, KC_UP,  KC_PGDN,_______,  KC_DEL},
   {KC_CAPS, KC_F11,  KC_F4,   KC_F5,   KC_F6,  KC_VOLU,  BL_INC,  KC_LEFT, KC_DOWN,KC_RGHT,_______,  KC_ENT },
   {KC_LSFT, KC_F12,  KC_F7,   KC_F8,   KC_F9,  KC_VOLD,  BL_DEC,  KC_HOME, KC_INS, KC_END, _______,  KC_RSFT},
-  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_LSFT,  KC_LSFT, RAISE,   LOWER,   KC_RALT, KC_APP, KC_RCTL}
+  {KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_LSFT,  KC_LSFT, RAISE,   LOWER, KC_PGUP, KC_PGDN, KC_RCTL}
 },
 
 /* Raise
@@ -122,14 +122,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  ^   |  &   |  |   |  ?   |  !   |  [   |  ]   |  7   |  8   |  9   |      | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  ~   |   %  |  `   | Lower|Space |  Shift      |Raise | Lower| Alt  |  App | Ctrl |
+ * |  ~   |   %  |  `   | Lower|Space |  Shift      |Raise | Lower|Pg Up |Pg Dn | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
   {KC_DLR,  KC_UNDS, KC_BSLS, KC_HASH,    KC_AT,   KC_LCBR, KC_RCBR, KC_1,    KC_2,    KC_3, KC_DOT, KC_DEL},
   {KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH,    KC_EQL,  KC_LPRN, KC_RPRN, KC_4,    KC_5,    KC_6, KC_0,   KC_ENT},
   {KC_CIRC, KC_AMPR, KC_PIPE, KC_QUESTION,KC_EXLM, KC_LBRC, KC_RBRC, KC_7,    KC_8,    KC_9, _______,KC_RSFT},
-  {KC_TILD, KC_PERC, KC_GRV,  LOWER,      KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,  KC_RALT,KC_APP,KC_RCTL}
+  {KC_TILD, KC_PERC, KC_GRV,  LOWER,      KC_SPC,  KC_LSFT, KC_LSFT, RAISE,   LOWER,KC_PGUP, KC_PGDN,KC_RCTL}
 },
 
 
