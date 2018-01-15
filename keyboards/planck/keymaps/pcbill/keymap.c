73afd8f1bb33621128233cb814c9b188f334cc07
Bill SJ Wu <Bill_SJ_Wu@wistron.com>
2018-01-15 15:28:34 +0800

    remove some multiple function keys

--- keyboards/planck/keymaps/pcbill/keymap.c
+++ keyboards/planck/keymaps/pcbill/keymap.c
@@ -100,14 +100,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+----- -+-------+---------+-----------+----+----+-----------+----------+--------+------------+------|
  * | Shift|   ;   |  Q    |   J     |   K       |X   |B   |   M       |   W      | V      |   Z        | Shift|
  * |------+-------+-------+---------+-----------+----+----+-----------+----------+--------+------------+------|
- * | Esc  |Esc/GUI|del/Alt|Tab/Lower|Space/Raise|Ent/Shift|Space/Raise|PgUp/Lower|PgDn/alt|bspc/Ctrl   | Ctrl |
+ * | Esc  |    GUI|    Alt|    Lower|Space/Raise|    Shift|Space/Raise|     Lower|PgUp/alt|  PgDn      | Ctrl |
  * `----------------------------------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
-  {KC_TAB, KC_QUOT,      KC_COMM,KC_DOT,      KC_P,        KC_Y,         KC_F,         KC_G,        KC_C,         KC_R,   KC_L,      KC_BSPC},
-  {KC_LCTL,KC_A,         KC_O,   KC_E,        KC_U,        KC_I,         KC_D,         KC_H,        KC_T,         KC_N,   KC_S,      KC_ENT },
-  {KC_LSFT,KC_SCLN,      KC_Q,   KC_J,        KC_K,        KC_X,         KC_B,         KC_M,        KC_W,         KC_V,   KC_Z,      KC_RSFT},
-  {KC_ESC, GUI_T(KC_ESC),ALT_T(KC_DEL),LT(5,KC_TAB),LT(4,KC_SPC),SFT_T(KC_ENT),SFT_T(KC_ENT),LT(4,KC_SPC),LT(5,KC_PGUP),ALT_T(KC_PGDN),CTL_T(KC_BSPC),KC_RCTL}
+  {KC_TAB, KC_QUOT, KC_COMM,KC_DOT,KC_P,        KC_Y,   KC_F,         KC_G,  KC_C,         KC_R,   KC_L,    KC_BSPC},
+  {KC_LCTL,KC_A,    KC_O,   KC_E,  KC_U,        KC_I,   KC_D,         KC_H,  KC_T,         KC_N,   KC_S,    KC_ENT },
+  {KC_LSFT,KC_SCLN, KC_Q,   KC_J,  KC_K,        KC_X,   KC_B,         KC_M,  KC_W,         KC_V,   KC_Z,    KC_RSFT},
+  {KC_ESC, KC_LGUI, KC_LALT,LOWER, LT(4,KC_SPC),KC_LSFT,KC_LSFT,LT(4,KC_SPC),LOWER, ALT_T(KC_PGUP),KC_PGDN, KC_RCTL}
 },
 
 /* Gherkin Dvorak
